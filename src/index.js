5724f5e53e8cc514f2dd6f1f1577ab85a5a7f6de
Loc <hoanglocst900@gmail.com>
2022-03-15 14:43:08 +0700

    ADD - routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
-import Movies from './components/movies';
+import App from './App';
 
 ReactDOM.render(
-  <main className='container'>
-    <Movies />
-  </main>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
