bfd83df1f1ed2f072ff3d4c7124b127af83e6019
Loc <hoanglocst900@gmail.com>
2022-04-04 12:29:36 +0700

    ADD - validate input fields

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -7,10 +7,29 @@ class LoginForm extends Component {
       username: "",
       password: "",
     },
+    errors: {},
+  };
+
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+
+    if (account.username.trim() === "") {
+      errors.username = "Username is required.";
+    }
+    if (account.password.trim() === "") {
+      errors.password = "Password is required.";
+    }
+
+    return errors;
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors });
+    if (errors) return;
 
     // call server here
     console.log("submit");
@@ -23,7 +42,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { username, password } = this.state.account;
+    const { account, errors } = this.state;
 
     return (
       <div className="">
@@ -32,13 +51,15 @@ class LoginForm extends Component {
           <Input
             name="username"
             onChange={this.handleChange}
-            value={username}
+            value={account.username}
+            error={errors.username}
             label="Username"
           />
           <Input
             name="password"
             onChange={this.handleChange}
-            value={password}
+            error={errors.password}
+            value={account.password}
             label="Password"
           />
           <button className="btn btn-primary">Login</button>
